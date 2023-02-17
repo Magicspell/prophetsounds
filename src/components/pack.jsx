b63f6139ca439fb91697cabff3ef4d2cf5730c80
Ralph Lewis <Magicspell@users.noreply.github.com>
2023-02-17 09:29:20 -0800

    Added price to packlist pages

--- src/components/pack.jsx
+++ src/components/pack.jsx
@@ -3,11 +3,17 @@ import { Link, redirect, useLoaderData } from "react-router-dom"
 import packData from "../database/packData.json"
 
 export default function Pack(props) {
+    const formatter = new Intl.NumberFormat('en-US', {style: "currency", currency: "USD"});
+    const priceString = formatter.format(packData[props["packId"]]["price"]);
+
     return (
         <Link className="text" to={"/packs/" + props["packId"]}>
             <div className="grid-item">
                 <img className="pack-art" src={packData[props["packId"]]["img"]}></img>
-                <p className="caption">{packData[props["packId"]]["name"]}</p>
+                <p className="caption">
+                    {packData[props["packId"]]["name"]}
+                    <span className="price">{priceString}</span>
+                </p>
             </div>
         </Link>
     )
